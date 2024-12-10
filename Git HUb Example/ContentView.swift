8fbf69dfcf40cf358bd04ccfbafd1f998b3caf1e
Marshall <marshalltolbert@yahoo.com>
2024-12-10 16:08:20 -0500

    Initial Commit

---
+++ Git HUb Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git HUb Example
+//
+//  Created by Marshall Tolbert on 12/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git HUb Example/Git_HUb_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_HUb_ExampleApp.swift
+//  Git HUb Example
+//
+//  Created by Marshall Tolbert on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Git_HUb_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
